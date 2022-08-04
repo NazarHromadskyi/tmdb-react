da954debbd79c75e5a37c03f97c8e966df4274f7
El Padlo <nazar.hromadskyi@gmail.com>
2022-08-05 00:38:42 +0300

    Tagline fix

--- src/pages/ContentDetails/MovieLayout.tsx
+++ src/pages/ContentDetails/MovieLayout.tsx
@@ -4,36 +4,40 @@ import { Badge, InfoField } from '../../components';
 import styles from './ContentDetails.module.scss';
 import { IMovieLayout } from './ContentDetails.types';
 
-export const MovieLayout: React.FC<IMovieLayout> = ({ data }) => (
-    <>
-        <div className={styles.info__right}>
-            <h1 className={styles.title}>{data.title}</h1>
-            <h4 className={styles.tagline}>{`«${data.tagline}»`}</h4>
-            <div className={styles.genres}>
-                {
-                    data.genres.map((genre) => (
-                        <Badge
-                            key={genre.id}
-                            isMatchNeeded={false}
-                        >{genre}
-                        </Badge>
-                    ))
-                }
+export const MovieLayout: React.FC<IMovieLayout> = ({ data }) => {
+    const isTagline = data.tagline.length > 0;
+
+    return (
+        <>
+            <div className={styles.info__right}>
+                <h1 className={styles.title}>{data.title}</h1>
+                {isTagline && <h4 className={styles.tagline}>{`«${data.tagline}»`}</h4>}
+                <div className={styles.genres}>
+                    {
+                        data.genres.map((genre) => (
+                            <Badge
+                                key={genre.id}
+                                isMatchNeeded={false}
+                            >{genre}
+                            </Badge>
+                        ))
+                    }
+                </div>
+                <div className={styles.fields}>
+                    <InfoField title="Status" value={data.status} />
+                    <InfoField title="Release date" value={data.release_date} />
+                    <InfoField title="Countries" arr={data.production_countries} field="name" />
+                    <InfoField title="Languages" arr={data.spoken_languages} field="english_name" />
+                    <InfoField title="Budget" value={data.budget} />
+                    <InfoField title="Revenue" value={data.revenue} />
+                    <InfoField title="Production" arr={data.production_companies} field="name" />
+                    <InfoField title="Collection" value={data.belongs_to_collection?.name} />
+                </div>
+                <div className={styles.overview}>
+                    <h2><span className={styles.fieldTitle}>Overview</span></h2>
+                    <p className={styles.overview__text}>{data.overview}</p>
+                </div>
             </div>
-            <div className={styles.fields}>
-                <InfoField title="Status" value={data.status} />
-                <InfoField title="Release date" value={data.release_date} />
-                <InfoField title="Countries" arr={data.production_countries} field="name" />
-                <InfoField title="Languages" arr={data.spoken_languages} field="english_name" />
-                <InfoField title="Budget" value={data.budget} />
-                <InfoField title="Revenue" value={data.revenue} />
-                <InfoField title="Production" arr={data.production_companies} field="name" />
-                <InfoField title="Collection" value={data.belongs_to_collection?.name} />
-            </div>
-            <div className={styles.overview}>
-                <h2><span className={styles.fieldTitle}>Overview</span></h2>
-                <p className={styles.overview__text}>{data.overview}</p>
-            </div>
-        </div>
-    </>
-);
+        </>
+    );
+};
